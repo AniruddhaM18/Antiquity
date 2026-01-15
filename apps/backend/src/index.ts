08e10a3b9987619a427fcb5370abb9022bfd982c
Aniruddha <aniruddhamaradwar9@gmail.com>
2026-01-15 12:00:15 +0530

    removed-betterAuth

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,12 +1,24 @@
 import dotenv from "dotenv";
 dotenv.config(); 
-import { toNodeHandler } from "better-auth/node";
-import { auth } from "./auth";
+import cors from "cors";
 import express from "express";
+import cookieParser from "cookie-parser";
 import contestRouter from "./routes/contestRoutes";
+import authRouter from "./routes/authRoutes";
+import participantRouter from "./routes/participantRouter";
 const app = express();
+const PORT = process.env.PORT!;
+//cors before routes
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true, //allow cookies,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+}))
+
 
 app.use(express.json());
+app.use(cookieParser());     
 
 //check route
 app.post("/health", (req, res)=> {
@@ -15,12 +27,14 @@ app.post("/health", (req, res)=> {
     })
 })
 
-//Auth - this time managed by betterauth
-app.all("/api/auth/*", toNodeHandler(auth));
+//auth routes
+app.use("/api/auth", authRouter);
 //contest rouetes bitch
 app.use("/api/", contestRouter);
+//participant routes
+app.use("/api", participantRouter);
 
 
-app.listen(3000, ()=> {
-    console.log("app listeing on port : 3000");
+app.listen(PORT, ()=> {
+    console.log(`app listeing on port : ${PORT}`);
 })
