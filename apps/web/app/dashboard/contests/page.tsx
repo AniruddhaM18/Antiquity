e0ca6961f706aa81ebee4fde6016b05ed086dc82
Aniruddha <aniruddhamaradwar9@gmail.com>
2026-02-06 22:02:57 +0530

    fixings

--- apps/web/app/dashboard/contests/page.tsx
+++ apps/web/app/dashboard/contests/page.tsx
@@ -7,10 +7,6 @@ import AttendedContestsSection from "@/src/components/admin/attendedContests";
 export default function DashboardContestsPage() {
   return (
     <div className="p-6 space-y-4">
-      <h1 className="text-xl font-semibold text-neutral-100">
-        Participated Contests
-      </h1>
-
       <AttendedContestsSection />
     </div>
   );
--- apps/web/app/dashboard/created/page.tsx
+++ apps/web/app/dashboard/created/page.tsx
@@ -6,10 +6,6 @@ import CreatedContestsSection from "@/src/components/admin/createdContest";
 export default function DashboardCreatedContestsPage() {
   return (
     <div className="p-6 space-y-4">
-      <h1 className="text-xl font-semibold text-neutral-100">
-        Created Contests
-      </h1>
-
       <CreatedContestsSection />
     </div>
   );
