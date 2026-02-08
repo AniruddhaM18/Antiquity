0556b896aeafe87232d3d2efe940c533fbfc4114
Aniruddha <aniruddhamaradwar9@gmail.com>
2026-02-08 19:28:17 +0530

    minor-fix

--- apps/web/app/auth/signup/page.tsx
+++ apps/web/app/auth/signup/page.tsx
@@ -1,16 +1,15 @@
-import { GalleryVerticalEnd } from "lucide-react"
-
 import { SignupForm } from "@/components/signup-form"
+import LogoIcon from "@/components/ui/logo"
 
 export default function SignupPage() {
   return (
-    <div className="bg-black flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-4" />
+       <div className="bg-black flex min-h-svh flex-col items-center justify-center gap-3 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-3">
+        <a href="#" className="flex justify-center items-center gap-1 self-center font-medium">
+          <div className=" text-primary-foreground size-8 flex items-center justify-center rounded-md">
+            <LogoIcon   />
           </div>
-          Antiquity Inc.
+          Antiquity
         </a>
         <SignupForm />
       </div>
